f523352af2d4f1684d0bafc43392a43dc265737d
resant18 <santoso.renata@gmail.com>
2019-09-16 16:37:34 -0700

    Completed Hash Set exercise

--- W5D1/hash_map_cache/lib/p03_hash_set.rb
+++ W5D1/hash_map_cache/lib/p03_hash_set.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class HashSet
   attr_reader :count
 
@@ -6,19 +8,33 @@ class HashSet
     @count = 0
   end
 
-  def insert(key)
+  def insert(key)    
+    return false if include?(key)
+
+    # insert first then check the size and resize it
+    self[key] << key
+    
+    @count += 1
+    resize! if num_buckets < self.count
+    true
+
   end
 
-  def include?(key)
+  def include?(key)            
+    self[key].include?(key) 
   end
 
   def remove(key)
+    @count -= 1 if self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    idx = num.hash % num_buckets
+    # puts "idx = #{idx}"
+    @store[idx]
   end
 
   def num_buckets
@@ -26,5 +42,15 @@ class HashSet
   end
 
   def resize!
+    #double the buckets amount
+    old_store = @store.dup
+    @store = Array.new(2 * num_buckets) { Array.new }
+
+    # reset before inserting the old data
+    @count = 0
+    # copy the old data to new resized buckets
+    old_store.flatten.each do |num|
+      insert(num)
+    end
   end
 end
